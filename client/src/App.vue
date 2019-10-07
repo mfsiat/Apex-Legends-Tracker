c61dde764bed0bbb731f20a98fbda49d5a191234
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-10-07 12:02:02 +0600

    added image on the header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container">
-    Hello
+    <Header />
   </div>
 </template>
 
 <script>
 
+import Header from './components/Header'
+
 export default {
   name: 'app',
   components: {
-    
+    Header
   }
 }
 </script>
