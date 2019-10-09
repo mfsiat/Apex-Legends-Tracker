45963e551b92051f327c6ecb5ea7a9bfa6744429
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-10-09 21:29:41 +0600

    configured the production built

--- server.js
+++ server.js
@@ -2,20 +2,28 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
-
 // Load env file
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
 
 // Creating the basic express server
 const app = express();
 
 // Dev logging
-if(process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Profile Routes
-app.use('/api/v1/profile', require('./routes/profile'));
+app.use("/api/v1/profile", require("./routes/profile"));
+
+// Handle production
+if (process.env.NODE_ENV === "production") {
+  // Set Static folder
+  app.use(express.static(__dirname + "/public/"));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
 
 // It will look at our port from the .env
 const port = process.env.PORT || 8000;
